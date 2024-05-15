cac26766d9ef97cc73ec0fe28cb0badfbf6338aa
Cauã Souza <caua_a_souza@estudante.sesisenai.org.br>
2024-05-14 22:00:36 -0300

    Updates Smart

---
+++ SMART/src/components/Busines.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Busines() {
+    return (
+        <section className='busines' id='busina'>
+            <img className='imgbusines' src="ParceirosChave.png"/>
+            <h1>Protótipo</h1>
+        </section>
+    )
+}
---
+++ SMART/src/components/Gallery.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Gallery(){
+    return(
+        <section className="Galeria">
+            
+            <img src="busca.jpg" />
+            <img src="comunicacao.jpg"/>
+            <img src="lobby.jpg"/>
+            <img src="login.jpg"/>
+            <img src="postagens.jpg" />
+            <img src="usuarios.jpg"/>
+        </section>
+    )
+}
