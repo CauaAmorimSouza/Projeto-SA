ecc995470419fb66866ac92929269d700df5ba43
Cauã Souza <caua_a_souza@estudante.sesisenai.org.br>
2024-05-14 21:50:01 -0300

    Smart

---
+++ S.M.A.R.T/smart/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <section id='fodd'>
+      <footer>
+        <p>© 2024 S.M.A.R.T. System. Todos os direitos reservados.</p>
+      </footer>
+    </section>
+  )
+}
+
+export default Footer
---
+++ S.M.A.R.T/smart/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Header(){
+    return(
+        <header>
+            <nav id="menu"> 
+            <ul className='navbar'> 
+                <li><a href="#busina">Business </a></li>
+                <li><a href="#sobre">sobre</a></li>
+                <li><a href="#fodd">Footer</a></li>
+                
+  </ul> 
+</nav>
+            <img src="capa.jpeg"/>
+        </header>
+    )
+}
